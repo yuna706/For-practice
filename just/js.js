85f0cec21eba0c02e21d30d56e92b70d32799fd4
yuna706 <riyu6027@naver.com>
2020-03-27 09:12:37 +0900

    just 쇼핑몰 완료

--- just/js.js
+++ just/js.js
@@ -7,6 +7,7 @@ $(function(){
         $(".submenu").stop().slideUp(500);
     });
 
+    // 슬라이드
     let imgs = $(".img"); //슬라이드 이미지 div를 저장
     let idx = 0; //현재 보고있는 슬라이드를 저장
 
@@ -18,4 +19,24 @@ $(function(){
         imgs.eq(idx).animate({"left":"-100%"},1000);
         idx = next;
     },3000);
+
+    // 탭 메뉴
+    $("#notice").click(function(){
+        $("#contents *").removeClass("on");
+        $("#notice").addClass("on");
+        $(".notice").addClass("on");
+    });
+    $("#gall").click(function(){
+        $("#contents *").removeClass("on");
+        $("#gall").addClass("on");
+        $(".gall").addClass("on");
+    });
+
+    // 팝업창
+    $(".notice> ul> li").eq(0).click(function(){
+        $(".popup").css("display","block");
+    });
+    $(".popup> button").click(function(){
+        $(".popup").css("display","none");
+    });
 })
